fc175162ef5288def468e884d313a5e98a2e66c2
CISSE MATHIEU <mathieu.cisse@etu.unistra.fr>
2020-12-04 15:25:12 +0100

    Authentification

--- config/routes.js
+++ config/routes.js
@@ -24,7 +24,7 @@ module.exports.routes = {
   'get /signup': { view: 'user/signup' },
   'post /login': 'UserController.login',
   'post /signup': 'UserController.signup',
-  '/logout': 'UserController.logout'
+  'get /logout': 'UserController.logout'
 
   /***************************************************************************
   *                                                                          *
